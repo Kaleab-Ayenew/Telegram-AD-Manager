e28f31b2a94346ae13a466cb60d61afdb37c45ba
Kaleab Ayenew <kaleab.ayenew2013@gmail.com>
2023-02-20 14:25:21 +0300

    Migrated database to postgresql

--- project_files/channels/models.py
+++ project_files/channels/models.py
@@ -8,7 +8,7 @@ class TelegramChannel(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     ch_username = models.CharField(max_length=150, unique=True)
     ch_id = models.IntegerField(unique=True)
-    ch_photo_id = models.CharField(max_length=300)
+    ch_photo_id = models.CharField(max_length=350)
     ch_member_count = models.IntegerField()
 
     def __str__(self):
