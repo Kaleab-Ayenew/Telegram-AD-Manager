52787bf558568725e285bd801f615265a15a8819
Kaleab Ayenew <kaleab.ayenew2013@gmail.com>
2023-04-13 09:43:21 +0300

    Changed the Database to fit the front-end | Added pagination functionality

--- project_files/main/models.py
+++ project_files/main/models.py
@@ -8,15 +8,35 @@ from datetime import datetime
 from channels.models import TelegramChannel
 
 
+def get_image_path(instance, filename):
+    ext = filename.split(".")[-1]
+    return f"post_images/{instance.unique_id}/image.{ext}"
+
+
+def get_img_default():
+    return "post_images/081340a8-385e-4c12-a97b-48a4821f20f2/image.png"
+
+
+def get_default_sdate():
+    return datetime.fromisoformat("2023-04-13")
+
+
+def get_default_edate():
+    return datetime.fromisoformat("2023-04-14")
+
+
 class ScheduledPost(models.Model):
     unique_id = models.UUIDField(default=uuid.uuid4, editable=False)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     destination_channel = models.ForeignKey(
         TelegramChannel, on_delete=models.CASCADE)
     post_content = models.TextField()
-    post_image_id = models.CharField(max_length=200, null=True)
+    post_image = models.ImageField(
+        upload_to=get_image_path, max_length=300, default=get_img_default)
     post_buttons = models.JSONField(blank=True, null=True)
-    schedules = models.JSONField()
+    start_date = models.DateField(default=get_default_sdate)
+    end_date = models.DateField(default=get_default_edate)
+    post_time = models.DateTimeField(default=datetime.now())
     created_at = models.DateTimeField(auto_now_add=True)
     sent = models.BooleanField(default=False)
 
--- project_files/tg_ad_manager/settings.py
+++ project_files/tg_ad_manager/settings.py
@@ -142,6 +142,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'static_files'
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media_files'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
