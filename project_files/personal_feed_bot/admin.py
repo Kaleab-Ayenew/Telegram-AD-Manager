f7624018c902f260277e8c6cb8511595d9d69726
Kaleab Ayenew <kaleab.ayenew2013@gmail.com>
2023-05-15 19:49:43 +0300

    Added feedchannels to the django admin

--- project_files/personal_feed_bot/admin.py
+++ project_files/personal_feed_bot/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import BotUser, ConnectedChannels, TempData
+from .models import BotUser, ConnectedChannels, TempData, FeedChannel
 
 
 # Register your models here.
 admin.site.register(BotUser)
 admin.site.register(ConnectedChannels)
 admin.site.register(TempData)
+admin.site.register(FeedChannel)
