5b71159c8a2dd64940f3b9613afb89ce9a128506
Kaleab Ayenew <kaleab.ayenew2013@gmail.com>
2023-06-08 16:39:20 +0300

    Feedgram minor look fixes

--- project_files/personal_feed_bot/utils.py
+++ project_files/personal_feed_bot/utils.py
@@ -351,8 +351,8 @@ def get_homepage_info(chat_id):
     sup_ch_no = len(feed_channels)
     join_list = lambda l, c='\n    ▫️ ': c.join(l)
     sup_ch_list = "\n".join(
-        [f' 🔸 {s_name} | {len(connected_ch[s_name])} channels\n    ▫️ {join_list(connected_ch[s_name])}\n' for s_name in connected_ch.keys()])
+        [f' 🔸 {s_name} | {len(connected_ch[s_name])} channels\n     {"▫️ "+join_list(connected_ch[s_name])}\n' for s_name in connected_ch.keys()])
 
-    home_page_info = f"Welcome back 💖 <b>{bot_user.user_first_name}</b> 💖\n\n❇️ <b>Super Channels: {sup_ch_no} channels</b>\n\n{sup_ch_list}\n{sub_info_text}"
+    home_page_info = f"Welcome back 💖 <b>{bot_user.user_first_name}</b>\n\n❇️ <b>Super Channels: {sup_ch_no} channels</b>\n\n{sup_ch_list}\n{sub_info_text}"
 
     return home_page_info
