2c02652161c80905f0c42e27b76fd4a52a184817
Kaleab Ayenew <kaleab.ayenew2013@gmail.com>
2023-06-10 00:36:42 +0300

    Feedgram: Minor spelling fix in profile info

--- project_files/personal_feed_bot/utils.py
+++ project_files/personal_feed_bot/utils.py
@@ -354,7 +354,7 @@ def get_homepage_info(chat_id):
         [f' 🔸 {s_name} | {len(connected_ch[s_name])} channels\n{join_list(connected_ch[s_name])}\n' for s_name in connected_ch.keys()])
 
     footer = "👉 ለተጨማሪ መረጃ ቻናላችንን ይቀላቀሉ:\n\nhttps://t.me/feedgram_info"
-    home_page_info = f"ሰላም 💖 <b>{bot_user.user_first_name}</b>\n\n❇️ <b>Super Channelኦች: {sup_ch_no} channels</b>\n\n{sup_ch_list}\n{sub_info_text}\n\n{footer}\n\n\n"
+    home_page_info = f"ሰላም 💖 <b>{bot_user.user_first_name}</b>\n\n❇️ <b>Super Channels: {sup_ch_no} channels</b>\n\n{sup_ch_list}\n{sub_info_text}\n\n{footer}\n\n\n"
 
     return home_page_info
 
